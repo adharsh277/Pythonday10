fd35fa570fe2010f7b1bab576eb798bed6cca7ee
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-26 19:55:47 +0530

    Add files via upload

---
+++ pythonday101.py
@@ -0,0 +1,60 @@
+
+# regional chunni
+x = "“Letting go means to come to the realization that some people are a part of your history, but not a part of your destiny"
+y = "When you play the game of thrones, you win or you die. There is no middle ground"
+print(x.upper())
+print(y.lower())
+print(x.lower())
+print(y.upper())
+print(x.replace("means", 'purnesh'))
+print(x.split())
+print(y.split())
+print(y.replace("go", "so"))
+
+print(type(x))
+print(type(y))
+print(x.replace("e", "o"))
+print(y.replace("r", "u"))
+print(y.replace("k", "p"))
+print(y.replace("h", "k"))
+print(y.replace("l", "r"))
+print(y.replace("y", "t"))
+print(y.replace("a", "q"))
+print(y.replace("t", "c"))
+print(y.replace("d", "k"))
+print(y.replace("k", "t"))
+print(x.split())
+print(x.split())
+print(300 == 56)
+print(562456+62672)
+print(26573-5623)
+print(7627-789065)
+print(7627*789065)
+print('lionel messi')
+print("cristano ronaldo")
+print("hathwayoiw", 'gayuyv', 'dgiefg', "dhigfj", 'kolghtrsgy', "dihfw")
+print(x)
+# python operators
+# arithemeticoperators
+print(10+10)
+print(78 == 874)
+print(2765**2)
+print(9766/2)
+print(83928//5)
+print(2633*765)
+mylist = ['apple', 'orange', 'gundapini', 'maripoi']
+mylist.clear()
+print(mylist)
+print(20/10)
+print(2000 % 3)
+print(15//2)
+a = [67, 98]
+b = [90, 79]
+c = a
+print(a is c)
+print(b is c)
+print(2 in a)
+print(a is c)
+x = 7
+print(x < 8 and x < 6)
+print(x > 3 and x > 1)
